1776b4403b319abc1d590ec6854caa01baac51cc
manug2 <manugarg2@gmail.com>
2015-05-08 05:53:03 +0800

    fixed code compilation warnings

--- src/main/com/open/algo/oanda/execution.py
+++ src/main/com/open/algo/oanda/execution.py
@@ -1,4 +1,3 @@
-import http.client
 import urllib
 import requests
 import json
@@ -15,8 +14,8 @@ class OandaExecutionHandler(ExecutionHandler):
             "Content-Type": "application/x-www-form-urlencoded"
             , "Authorization": "Bearer " + access_token
         }
-        self.url = domain + '/v1/accounts/%s/orders' % (account_id)
-        if logEnabled == True:
+        self.url = domain + '/v1/accounts/%s/orders' % account_id
+        if logEnabled:
             import logging
 
             self.logger = logging.getLogger(self.__class__.__name__)
@@ -37,22 +36,22 @@ class OandaExecutionHandler(ExecutionHandler):
 
     def parseResponse(self, response):
         # if self.logger != None:
-        #self.logger.debug ('Response from execution server : "%s"' % response)
+        # self.logger.debug ('Response from execution server : "%s"' % response)
 
-        if response == None:
+        if response is None:
             return {'code': -1, 'message': 'No response from Oanda execution server'}
-        elif response.content == None:
+        elif response.content is None:
             return {'code': -1, 'message': 'No response from Oanda execution server'}
         else:
             content = response.content.decode('utf-8')
-            #put this into journal
-            if content == None:
+            # put this into journal
+            if content is None:
                 return {'code': -1, 'message': 'No response from Oanda execution server'}
             else:
-                #if self.logger != None:
-                #self.logger.debug('Response from execution server : "%s"' % content)
+                # if self.logger != None:
+                # self.logger.debug('Response from execution server : "%s"' % content)
                 content = json.loads(content)
-                if self.logger != None:
+                if self.logger is not None:
                     self.logger.info('Response parsed : "%s"' % content)
                 return content
 
@@ -60,11 +59,11 @@ class OandaExecutionHandler(ExecutionHandler):
         self.executing = False
 
     def execute_order(self, event):
-        if self.logger != None:
+        if self.logger is not None:
             self.logger.debug(event)
 
-        if self.executing == False:
-            if self.logger != None:
+        if not self.executing:
+            if self.logger is not None:
                 self.logger.warn('Executor in stop mode, will not execute order: "%s"' % event)
             return
 
@@ -76,13 +75,13 @@ class OandaExecutionHandler(ExecutionHandler):
         }
         for attr in ['price', 'lowerBound', 'upperBound', 'stopLoss', 'takeProfit', 'trailingStop', 'expiry']:
             value = getattr(event, attr)
-            if value != None:
+            if value is not None:
                 params[attr] = value
 
-        request_args = {}
+        request_args = dict()
         request_args['data'] = urllib.parse.urlencode(params)
 
-        if self.logger != None:
+        if self.logger is not None:
             self.logger.debug('URL:%s, PARAM:%s' % (self.url, request_args))
 
         func = getattr(self.session, 'post')
@@ -90,30 +89,29 @@ class OandaExecutionHandler(ExecutionHandler):
             response = func(self.url, **request_args)
             return self.parseResponse(response)
         except requests.RequestException as e:
-            if self.logger != None:
+            if self.logger is not None:
                 self.logger.error(e)
             # put in exception queue
             return {'code': -2, 'message': str(e)}
 
-
     def stop(self):
         self.executing = False
-        if self.session != None:
+        if self.session is not None:
             self.session.close()
         self.session = None
 
     def get_orders(self, params=None):
         # def get_orders(self, order_type="market", instrument=None):
-        if self.executing == False:
-            if self.logger != None:
+        if not self.executing:
+            if self.logger is not None:
                 self.logger.warn('Executor in stop mode, will not query orders')
             return
 
         request_args = {}
-        if params != None:
+        if params is not None:
             request_args['data'] = urllib.parse.urlencode(params)
 
-        if self.logger != None:
+        if self.logger is not None:
             self.logger.debug('URL:%s, PARAM:%s' % (self.url, request_args))
 
         func = getattr(self.session, 'get')
@@ -121,22 +119,22 @@ class OandaExecutionHandler(ExecutionHandler):
             response = func(self.url, **request_args)
             return self.parseResponse(response)
         except requests.RequestException as e:
-            if self.logger != None:
+            if self.logger is not None:
                 self.logger.error(e)
-            #put in exception queue
+            # put in exception queue
             return {'code': -2, 'message': str(e)}
 
     # end of get_orders
 
     def get_order(self, order_id):
-        if self.executing == False:
-            if self.logger != None:
+        if not self.executing:
+            if self.logger is not None:
                 self.logger.warn('Executor in stop mode, will not query order')
             return
 
         request_args = {}
         url = '%s/%s' % (self.url, order_id)
-        if self.logger != None:
+        if self.logger is not None:
             self.logger.debug('URL:%s' % url)
 
         func = getattr(self.session, 'get')
@@ -144,9 +142,9 @@ class OandaExecutionHandler(ExecutionHandler):
             response = func(url, **request_args)
             return self.parseResponse(response)
         except requests.RequestException as e:
-            if self.logger != None:
+            if self.logger is not None:
                 self.logger.error(e)
-            #put in exception queue
+            # put in exception queue
             return {'code': -2, 'message': str(e)}
-            #end of get_order
+            # end of get_order
 
