757146fb4e36ec5efead49b2076494ee50bf69b1
manug2 <manugarg2@gmail.com>
2015-06-02 11:41:13 +0800

    moved event-loop-queue based wiring implementation into new test folder

--- src/test/queueWired/TestFxCostPredictor.py
+++ src/test/queueWired/TestFxCostPredictor.py
@@ -44,40 +44,10 @@ class TestCostPrediction(unittest.TestCase):
         except KeyError:
             pass
 
-    def test_should_be_able_to_pull_two_ticks_from_event_queue(self):
-        tick1 = TickEvent('CHF_USD', get_time(), 1.0, 1.0)
-        tick2 = TickEvent('EUR_USD', get_time(), 1.0, 1.0)
-        self.events.put_nowait(tick1)
-        self.events.put_nowait(tick2)
-        self.looper.pull_process()
-        self.assertEqual(self.cost_predictor.get_last_tick(), tick2)
-
-    def test_should_be_able_to_pull_two_ticks_of_same_instrument_from_event_queue(self):
-        tick1 = TickEvent('CHF_USD', get_time(), 1.0, 1.0)
-        tick2 = TickEvent('CHF_USD', get_time(), 1.01, 1.02)
-        self.events.put_nowait(tick1)
-        self.events.put_nowait(tick2)
-        self.looper.pull_process()
-        last_tick = self.cost_predictor.get_last_tick()
-        self.assertEqual(last_tick.instrument, tick2.instrument)
-        self.assertEqual(last_tick.bid, tick2.bid)
-        self.assertEqual(last_tick.ask, tick2.ask)
-
-    def test_should_eval_cost_if_one_tick_available(self):
+    def test_should_eval_cost_if_tick_available(self):
         tick = TickEvent('CHF_USD', get_time(), 1.01, 1.02)
         self.events.put_nowait(tick)
         self.looper.pull_process()
         order = OrderEvent(tick.instrument, 100, 'buy')
         cost = self.cost_predictor.eval_cost(order)
         self.assertEqual(0.01, cost)
-
-    def test_should_eval_cost_if_two_ticks_available(self):
-        tick1 = TickEvent('CHF_USD', get_time(), 1.01, 1.03)
-        tick2 = TickEvent('CHF_USD', get_time(), 1.02, 1.03)
-        self.events.put_nowait(tick1)
-        self.events.put_nowait(tick2)
-        self.looper.pull_process()
-        order = OrderEvent(tick2.instrument, 100, 'buy')
-        cost = self.cost_predictor.eval_cost(order)
-        self.assertEqual(0.015, cost)
-
